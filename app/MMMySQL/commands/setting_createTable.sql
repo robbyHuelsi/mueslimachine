fd2b18495c4f570494540312e59eac114030c3c8
Robert Hülsmann <robert@huelsi.de>
2020-07-27 00:23:09 +0200

    CURRENT_TIMESTAMP instead now()

--- app/MMMySQL/commands/setting_createTable.sql
+++ app/MMMySQL/commands/setting_createTable.sql
@@ -3,7 +3,7 @@ CREATE TABLE `{table}`
     setting_uid      BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     setting_key      VARCHAR(50)     NOT NULL,
     setting_value    VARCHAR(255),
-    setting_reg_date TIMESTAMP       NOT NULL DEFAULT now(),
-    setting_updated  TIMESTAMP ON UPDATE now(),
+    setting_reg_date TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    setting_updated  TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     UNIQUE (`setting_uid`, `setting_key`)
 ) ENGINE = INNODB
